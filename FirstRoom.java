d78666feae1e6778930145b9150f101033d3a43f
nuozrok <1korzoun1@gmail.com>
2015-10-02 22:02:22 +0000

    FirstRoom
    Added the event to occur at the first room. Create character, room description, etc...

---
+++ FirstRoom.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FirstRoom{
+    public static void main(String[] args){
+        
+        //Description of the room
+        System.out.println("");
+    
+        System.out.println("But who are you?");
+        System.out.println("What is your race?");
+        System.out.println("Human\nDwarf\nHalfling\nElf\nOrc");
+        
+    }
+}
