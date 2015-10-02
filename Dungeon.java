25408bb3a6381b899a625bd35bebb0e2353f4103
whig123 <mwong940@gmail.com>
2015-10-02 22:32:07 +0000

    fix up generate
    generate is shaping up with a good looking RNG, need to implement changeRoom()

--- Dungeon.java
+++ Dungeon.java
@@ -1,22 +1,25 @@
+import java.lang.*;
 
 public class Dungeon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		public static final int maxRooms = 10;
+	    public static final Room playerRoom;
+		public static final int maxRooms = 9;
 	}
 
 	// Create First Room
 	public static void Start() {
-	    
-	    // this is commited again again
+	   
+	   
 		Room firstRoom = new Room();
-		Room rooms(maxRooms);
+	    playerRoom = firstRoom;
+		Room rooms[maxRooms];
 		firstRoom.id = 0;
 		firstRoom = rooms(0);
 
 	}
-	//
+	
 
 	// Generate Rooms
 	public static void Generate() {
@@ -29,39 +32,43 @@ public class Dungeon {
 			currentRoom.id = roomCount;
 
             // which direction will the door be (don't forget to check that door isn't already there)
-			switch (RNG.D4()) {
-			case 1:
-			    // forward door
-				rooms(roomCount - 1).canMoveNorth() = currentRoom.id;
-				
-				// backwards door
-                currentRoom.canMoveSouth() = rooms(roomCount - 1).id;
-				break;
-			case 2:
-                rooms(roomCount - 1).canMoveEast() = currentRoom.id; 
-                
-                currentRoom.canMoveWest() = rooms(roomCount - 1).id;
-				break;
-			case 3:
-                rooms(roomCount - 1).canMoveWest() = currentRoom.id;
-                currentRoom.canMoveEast() = rooms(roomCount - 1).id;
-			
-				break;
-			case 4:
-                rooms(roomCount - 1).canMoveSouth() = currentRoom.id;  
-			    currentRoom.canMoveNorth() = rooms(roomCount - 1).id;
-			
-				break;
-			}
+
+        	// 4. randomly add a door to the wall
+    			switch (RNG.D4()) {
+    			case 1:
+    			    // forward door
+    				rooms[roomCount - 1].canMoveNorth = currentRoom.id;
+    				currentRoom.description += "\n there is a room to the north of you";  
+    				
+    				// backwards door
+                    currentRoom.canMoveSouth = rooms[roomCount - 1].id;
+    				currentRoom.description += "\n there is a room to the south of you";
+    				break;
+    			case 2:
+                    rooms[roomCount - 1].canMoveEast = currentRoom.id; 
+    				currentRoom.description += "\n there is a room to the east of you";
+    				
+                    currentRoom.canMoveWest = rooms[roomCount - 1].id;
+    				currentRoom.description += "\n there is a room to the west of you";
+    				break;
+    			case 3:
+                    rooms[roomCount - 1].canMoveWest = currentRoom.id;
+    				currentRoom.description += "\n there is a room to the east of you";
+    				
+                    currentRoom.canMoveEast = rooms[roomCount - 1].id;
+    				currentRoom.description += "\n there is a room to the west of you";    			
+    				break;
+     			case 4:
+                    rooms[roomCount - 1].canMoveSouth = currentRoom.id;  
+    				currentRoom.description += "\n there is a room to the south of you";
+    				
+    			    currentRoom.canMoveNorth = rooms[roomCount - 1].id;
+    				currentRoom.description += "\n there is a room to the north of you";
+    			
+    				break;
+    			}
 			
 		}
-		// 2. pick a room
-		// 3. choose a wall
-		// 4. randomly add a door to the wall, or do not
-		// 5. repeat steps 2 and 3, marxRooms more times, such that there is at least 1
-		// door per room
-		// 6. add a room adjacent to each door, unless a room already exists
-
 	}
 	//
 
