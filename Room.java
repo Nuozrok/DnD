41c9644ced6dcbb627bc12a2a5bf7148087f27e3
whig123 <mwong940@gmail.com>
2015-10-06 19:23:14 +0000

    fixing doors again
    when you forget to save :(

--- Room.java
+++ Room.java
@@ -9,11 +9,11 @@ public class Room {
 	public static int id;
 	public static String roomName;
 	public static String description;
-	public static boolean canMoveNorth;
-	public static boolean canMoveEast;
-	public static boolean canMoveSouth;
-	public static boolean canMoveWest;
-	public static boolean stairCaseUp;
+	public static int canMoveNorth = -1;
+	public static int canMoveEast = -1;
+	public static int canMoveSouth = -1;
+	public static int canMoveWest = -1;
+	public static int stairCaseUp = -1;
 	public static boolean stairCaseDown;
 
 }
