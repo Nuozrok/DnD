06f75f1ca0e22c05bbd901470af8356a7eba318c
nuozrok <1korzoun1@gmail.com>
2015-10-19 14:37:52 +0000

    Inventory
    started writing for the inventory textIO, etc..

--- Gear.java
+++ Gear.java
@@ -1,5 +1,23 @@
 public class Gear{
     public static void main(String[] args){
-        
+        type();
+        //if(Inventory.txt contains [insert item name here]){
+        //    PlayerStats.whatever stat += some modifier;
+        //}
+    }
+    //define what slot the gear is associted switch
+    public static void type(){
+        //types of armor that can be equipped
+        String headgear[] = new String[20];
+	    String chestpiece[] = new String[20];
+	    String bracers[] = new String[20];
+	    String gloves[] = new String[20];
+	    String ring[] = new String[20];
+	    String leggings[] = new String[20];
+	    String boots[] = new String[20];
+	    String weapon[] = new String[20];
+	    
+	    //gear available in the game
+	    ring[0] = "testRing";
     }
 }
--- Inventory.java
+++ Inventory.java
@@ -4,16 +4,19 @@ import java.io.*;
 public class Inventory {
 
 	public static void main(String[] args) {
+        Gear.main(null);
+        content();
         boolean loop = true;
         while(loop == true){
             Scanner input = new Scanner(System.in);
             System.out.println("====================");
             System.out.println("Check or equip?");
             System.out.println("====================");
-            if(input.next().equalsIgnoreCase("check")){
+            String response = input.next(); 
+            if(response.equalsIgnoreCase("check")){
                 loop = false;
                 checkContent();
-            }else if(input.next().equalsIgnoreCase("equip")){
+            }else if(response.equalsIgnoreCase("equip")){
                 loop = false;
                 equip();
             }
@@ -21,24 +24,19 @@ public class Inventory {
 	}
 	//items currently in your inventory
 	public static void content(){
-	    TextIO.write("inventory.txt");
+	    TextIO.writeFile("Inventory.txt");
+	    //TextIO.put("test");
 	}
 	//Print items in your inventroy and their descriptions 
 	public static void checkContent(){
-	
+	    System.out.println("You have:");
+	    TextIO.readFile("Inventory.txt");
+	    String inv = TextIO.getln();
+	    TextIO.writeStandardOutput();
+	    TextIO.putln(inv);
 	}
 	//equip gear in your inventory
 	public static void equip(){
-	    //types of armor that can be equipped
-	    Gear headgear = new Gear();
-	    Gear chestpiece = new Gear();
-	    Gear bracers = new Gear();
-	    Gear gloves = new Gear();
-	    Gear ring = new Gear();
-	    Gear leggings = new Gear();
-	    Gear boots = new Gear();
-	    Gear weapon = new Gear();
-	 
 	    Scanner input = new Scanner(System.in);
 	    System.out.println("====================");
 	    System.out.println("What would you like to equip?");
@@ -49,11 +47,12 @@ public class Inventory {
 	    while(loop = true){
 	        if(response.equalsIgnoreCase("headgear")){
 	            System.out.println("====================");
-	            //which headgear to equip?
-	            System.out.println("");
-	            //list headgear available
+	            System.out.println("Which headgear would you like to equip?");
+	            //if item is headgear and in your inventory, then list it as a choice
+	            //print choices
 	            System.out.println();
 	            System.out.println("====================");
+	            //if choice was x, then equip x and print that x was equipped
 	            System.out.println("back");
 	        }else if(response.equalsIgnoreCase("chestpiece")){
 	        
