decee43d0b8ab0b534a4b3a05e9803ebac8c098d
nuozrok <1korzoun1@gmail.com>
2015-10-22 20:20:27 +0000

    Inventory
    fixed

--- Inventory.java
+++ Inventory.java
@@ -24,7 +24,7 @@ public class Inventory {
 	}
 	//items currently in your inventory
 	public static void content(){
-	    TextIO.writeFile("Inventory.txt");
+	    //TextIO.writeFile("Inventory.txt");
 	    //TextIO.put("test");
 	}
 	//Print items in your inventroy and their descriptions 
@@ -37,29 +37,32 @@ public class Inventory {
 	}
 	//equip gear in your inventory
 	public static void equip(){
-	    Scanner input = new Scanner(System.in);
-	    System.out.println("====================");
-	    System.out.println("What type of gear would you like to equip?");
-	    System.out.println("headgear\nchestpiece\nbracers\ngloves\nring\nleggings\nboots\nweapon");
-	    System.out.println("====================");
-	    System.out.println("back");
-	    String response = input.next();
 	    boolean loop = true;
-	    while(loop = true){
+	    while(loop == true){
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("====================");
+	        System.out.println("What type of gear would you like to equip?");
+	        System.out.println("headgear\nchestpiece\nbracers\ngloves\nring\nleggings\nboots\nweapon");
+	        System.out.println("====================");
+	        System.out.println("back");
+	        String response = input.next();
 	        if(response.equalsIgnoreCase("headgear")){
 	            loop  = false;
 	            System.out.println("====================");
 	            System.out.println("Which headgear would you like to equip?");
+	            ReadingFiles.main(null);
 	            String fileName = "Inventory.txt";
                 String buffer;
 	            //list headgear currently in inventory, their stats, and what is currently equipped
                 TextIO.readFile(fileName);
+                TextIO.writeStandardOutput();
                 while(TextIO.peek() != TextIO.EOF){
                     buffer = TextIO.getln();
                     TextIO.putf("%s\n", buffer);
                 }
-                TextIO.readStandardInput();
 	            System.out.println("====================");
+	            
+                TextIO.readStandardInput();
 	            //if choice was x, then equip x and print that x was equipped
 	        }else if(response.equalsIgnoreCase("chestpiece")){
 	        
@@ -76,6 +79,7 @@ public class Inventory {
 	        }else if(response.equalsIgnoreCase("weapon")){
 	        
 	        }else if(response.equalsIgnoreCase("back")){
+	            loop = false;
 	            break;
 	        }         
 	    }
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # DnD
 
-Reworking inventory - yay text IOs
-\n\tSpeaking of inventory... it will break koding if you try to run it.
-dont even try. Its broken. Im trying to fix it. 
-\n\t also need to find a way to indicate if something in your inventory is currently equipped.
-perhaps print it in bold? Shouldnt be that difficult to do.
-\nMaking gear that as of now is just stat modifiers
+Reworking inventory - yay text IOs. 
+Also need to find a way to indicate if something in your inventory is currently equipped. 
+Perhaps print it in bold? Shouldnt be that difficult to do. 
+Making gear that as of now is just stat modifiers
